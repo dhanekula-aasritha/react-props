39319f9e383dc9ec9adf1daf963679f8aff89bf0
dhanekula-aasritha <aasrithadhanekula9999@gmail.com>
2022-11-30 16:51:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+        <PropsExample name="aasritha" age="20"/>
+        <PropsExample name="bhagi" age="16"/>
+        {/*<p>this is laptop</p>
+                <h1>TEDDY BEAR</h1>
+                <img src="https://img.freepik.com/free-vector/watercolor-bear-set_439185-6917.jpg?w=2000" width="30%" alt="teddy bear"/>
+                <ClassComponent/>*/}
+    </section>     
+    );
 }
-
 export default App;
---
+++ src/ClassComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class ClassComponent extends React.Component{
+	render(){
+		return(
+			<div>
+				i like teddy bear
+			</div>
+			)
+	}
+}
+export default ClassComponent;
